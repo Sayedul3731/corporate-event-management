15cd933fd7da9185bdf3bdd34e28cf38c47ac486
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-10-07 08:37:49 -0700

    Banner title customize

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -5,12 +5,8 @@ const Banner = () => {
         <div className="w-full h-[600px] banner my-10">
             <div>
                 <h1 className="text-5xl text-white font-bold text-center">Welcome <br /> to <br /> our event management <br /> site.</h1>
-                <h2 className="text-3xl text-white font-medium text-center">We provide technology event services. We are committed to providing sustainable services. Visit our site for details.</h2>
+                <h2 className="text-3xl text-white font-medium text-center">We provide technology event services. We are committed to providing best services. Visit our site for details...</h2>
             </div>
-            <div>
-
-            </div>
-
         </div>
     );
 };
