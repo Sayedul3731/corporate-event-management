ab98ef1b58d04903e25404264fbe7672ccbed2db
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-10-06 21:32:13 -0700

    Navbar created

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -15,19 +15,21 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-medium">
                         {navLinks}
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <div>
+                    <img className="w-[200px]" src="/src/assets/logo.png" alt="" />
+                </div>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1 font-medium">
                     {navLinks}
                 </ul>
             </div>
             <div className="navbar-end">
-                <NavLink><button className="bg-blue-500 px-5 py-2 rounded-sm font-bold font-Gabarito">Login</button></NavLink>
+                <NavLink to="/login"><button className="bg-blue-700 text-white px-5 py-2 rounded-sm font-bold font-Gabarito">Login</button></NavLink>
             </div>
         </div>
     );
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -3,7 +3,7 @@ import Navbar from "../Header/Navbar";
 
 const Root = () => {
     return (
-        <div>
+        <div className="max-w-[1250px] mx-auto font-Gabarito">
             <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
